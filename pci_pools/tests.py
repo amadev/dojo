62a3f3cc8b65c9b17804cadfeffc3723b24a16c7
Andrey Volkov <avolkov@mirantis.com>
2017-05-05 17:33:40 +0300

    Test non unique and overlapped requests

--- pci_pools/tests.py
+++ pci_pools/tests.py
@@ -16,3 +16,32 @@ def test_get_two_pci():
     updated_requests = pick(pools, requests, unique_tags)
     assert {'physnet': 'net_A', 'switch': 'sw_B', 'pf': 'pf_3'} in updated_requests
     assert  {'physnet': 'net_B', 'switch': 'sw_A', 'pf': 'pf_4'} in updated_requests
+
+
+def test_fail_for_non_unique():
+    requests = [
+        {'physnet': 'net_A'},
+        {'physnet': 'net_B'}
+    ]
+    pools = [
+        {'physnet': 'net_A', 'switch': 'sw_A', 'pf': 'pf_3'},
+        {'physnet': 'net_B', 'switch': 'sw_A', 'pf': 'pf_4'},
+    ]
+    unique_tags = ['switch', 'pf']
+    assert None == pick(pools, requests, unique_tags)
+
+
+def test_overlapped_requests():
+    requests = [
+        {'physnet': 'net_A'},
+        {'vendor': 'abc'}
+    ]
+    pools = [
+        {'subnet': 'blue', 'physnet': 'net_A'},
+        {'subnet': 'red', 'physnet': 'net_A', 'vendor': 'abc'},
+        {'subnet': 'blue', 'vendor': 'abc'},
+    ]
+    unique_tags = ['subnet']
+    updated_requests = pick(pools, requests, unique_tags)
+    assert {'physnet': 'net_A', 'subnet': 'blue'} in updated_requests
+    assert {'vendor': 'abc', 'subnet': 'red'} in updated_requests
