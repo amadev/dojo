e280f77fc7fb4b3723ce138eb165f57722a8746c
Andrey Volkov <avolkov@mirantis.com>
2017-05-19 14:01:03 +0300

    Add check_prime.py

---
+++ check_prime.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def isPrime(self, A):
+        if A == 1:
+            return 0
+        upperLimit = int(A**0.5)
+        for i in xrange(2, upperLimit + 1):
+            if i < A and A % i == 0:
+                return 0
+        return 1
+
+
+s = Solution()
+print s.isPrime(2)
